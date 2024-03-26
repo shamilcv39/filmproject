6a997e5355ae83bb6c4c65648673de9aba128fb7
shamilcv39 <152262779+shamilcv39@users.noreply.github.com>
2024-03-26 16:55:21 +0530

    Update urls.py

--- filmproject/urls.py
+++ filmproject/urls.py
@@ -18,11 +18,13 @@ from django.contrib import admin
 from django.urls import path,include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include("filmapp.urls")),
     path('registerapp/',include('registerapp.urls')),
+    path('accounts/login/', auth_views.LoginView.as_view(), name='login'),
 ]
 
 if settings.DEBUG:
